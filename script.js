3a1e0ee1961eb0bdde5e490917b0386917a68b0d
Alcohol Based Based <70607699+alcobased@users.noreply.github.com>
2023-02-14 21:33:01 +0000

    buttons

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
 	addBox(document.body, 50, 50, 100, 10, "blue");
 	addBox(document.body, 50, 50,  10, 100, "green");
 	const pairs = getBoxPairs(document.querySelectorAll(".box"));
-	console.log(pairs);
+	addButtons(document.body, 3);
+	
 });
 
 function addBox(container, width, height, posX, posY, color) {
@@ -31,13 +32,12 @@ function drawLines(boxes) {
 
 function getBoxPairs(boxes) {
 	const boxPairs = [];
-	for (const box1 of boxes) {
-		for (const box2 of boxes) {
-			if (box1 != box2) {
-				boxPairs.push([box1, box2]);
-			}
+	for (let i = 0; i < boxes.length; i++) {
+		for (let j = i + 1; j < boxes.length; j++) {
+			boxPairs.push([boxes[i], boxes[j]]);
 		}
 	}
+	console.log(boxPairs[0]);
 	return boxPairs;
 }
 
@@ -56,4 +56,31 @@ function getBox(width, height, posX, posY, color) {
 	box.style.top = `${posY}px`;
 	box.style.backgroundColor = color;
 	return box
+}
+
+function changeColor(box, color) {
+	box.style.backgroundColor = color;
+}
+
+function changeColors(boxes, colors) {
+	for (let i = 0; i < boxes.length; i++) {
+		changeColor(boxes[i], colors[i]);
+	}
+}
+
+function getRandomColor() {
+	return `#${(Math.floor(Math.random() * 16777216)).toString(16)}`;
+}
+
+function getRandomColorPair() {
+	return [getRandomColor(), getRandomColor()];
+}
+
+function addButtons(container, count, pairs) {
+	for (let i = 0; i < count; i++) {
+		const button = document.createElement("button");
+		button.innerText = `Pair ${i + 1}`;
+		button.add
+		container.appendChild(button);
+	}
 }
